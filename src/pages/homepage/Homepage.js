920ab802d3e504c612b3c7094c726f6eb78bc2e0
Luchi-Rach <84988864+Luchi-Rach@users.noreply.github.com>
2023-02-20 12:38:38 +0530

    home

--- src/pages/homepage/Homepage.js
+++ src/pages/homepage/Homepage.js
@@ -21,33 +21,38 @@ const Homepage = () => {
     navigate("../signin");
   };
   return (
-    <div className="homepage row">
-      <div className="col-xl-6 col-lg-6 col-md-12 banner-left">
-        <div className="banner-text">
-          Find the job that fits your life
-        </div>
-        <div className="banner-actions">
-          <div className="recruiter-action" onClick={recruiterHandler}>
-            I'm a Recruiter
+    <>
+      <div className="homepage row">
+        <div className="col-xl-6 col-lg-6 col-md-12 banner-left">
+          <div className="banner-text">
+            Find the job that fits your life
           </div>
-          <div className="candidate-action" onClick={candidateHandler}>
-            I'm looking for Job
+          <div className="banner-actions">
+            <div className="recruiter-action" onClick={recruiterHandler}>
+              I'm a Recruiter
+            </div>
+            <div className="candidate-action" onClick={candidateHandler}>
+              I'm looking for Job
+            </div>
+          </div>
+        </div>
+        <div className="col-xl-6 col-lg-6 col-md-12 banner-right">
+          <div className="banner-right-content">
+            <div className="img-bg-circle-area">
+              <div className="img-bg-circle1 rotate-center"></div>
+              <div className="img-bg-circle2 rotate-center-reverse"></div>
+              <div className="img-bg-circle3"></div>
+            </div>
+            <div className="user-img">
+              {/* <img src={imagePath} alt="user" /> */}
+            </div>
           </div>
         </div>
       </div>
-      <div className="col-xl-6 col-lg-6 col-md-12 banner-right">
-        <div className="banner-right-content">
-          <div className="img-bg-circle-area">
-            <div className="img-bg-circle1 rotate-center"></div>
-            <div className="img-bg-circle2 rotate-center-reverse"></div>
-            <div className="img-bg-circle3"></div>
-          </div>
-          <div className="user-img">
-            {/* <img src={imagePath} alt="user" /> */}
-          </div>
-        </div>
+      <div className="footer">
+        Lilly does NOT call or charge any fees for hiring at any stage. Be aware of Fraudsters or impersonators asking for the same. In case you are in doubt, or want to report a complaint reach out to legal@lilly.com
       </div>
-    </div>
+    </>
   );
 };
 
